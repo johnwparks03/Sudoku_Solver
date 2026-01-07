225f90a95d8de1fdf89c926bf5630aa338624cd0
John Parks <johnwparks03@gmail.com>
2026-01-06 20:48:43 -0500

    Added an id to the sudoku cell

--- SudokuCell.py
+++ SudokuCell.py
@@ -2,6 +2,7 @@ class SudokuCell:
     def __init__(self, row, col):
         self.row = row
         self.col = col
+        self.id = (row * 10) + col
         self.value = 0 # 0 indicates an empty cell
         self.possible_values = set(range(1, 10)) # Possible values from 1 to 9
         self.constraints = [] # List of constraint objects affecting this cell
@@ -10,3 +11,7 @@ class SudokuCell:
         self.value = value
         for constraint in self.constraints:
             constraint.propagate()
+
+    def add_constraint(self, constraint):
+        self.constraints.append(constraint)
+        constraint.propagate()
